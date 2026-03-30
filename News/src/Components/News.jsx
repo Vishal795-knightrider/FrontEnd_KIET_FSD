38de4aec56022ee3a77011a4cda278da618cdaf4
Vishal795-knightrider <vk3293801@gmail.com>
2026-03-30 15:44:34 +0530

    updated News app

--- News/src/Components/News.jsx
+++ News/src/Components/News.jsx
@@ -2,24 +2,23 @@ import { useState, useEffect } from "react";
 
 export default function News() {
   const [news, setNews] = useState([]);
-  const [search,setSearch]=useState("tesla");
+  const [search, setSearch] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  //state in pagination
-  const [currentPage,setCurrentPage]=useState(1);
-  const itemPerPage=6;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemPerPage = 6;
 
   function fetchdata() {
     setLoading(true);
+    setCurrentPage(1);
 
     fetch(
-      "https://newsapi.org/v2/everything?q=${search}&from=2026-02-23&sortBy=publishedAt&apiKey=dd87f40200304e7289285277a13c8071")
+      `https://newsapi.org/v2/everything?q=${search}&apiKey=4adb99b0db754b89af23f7d156d4afee`
+    )
       .then((res) => res.json())
       .then((data) => {
-        if (data.status !== "ok") {
-          throw new Error("API Error");
-        }
-
+        if (data.status !== "ok") throw new Error();
         setNews(data.articles || []);
         setLoading(false);
       })
@@ -33,33 +32,38 @@ export default function News() {
     fetchdata();
   }, []);
 
-const lastIndex=itemPerPage*currentPage;
-const firstIndex=lastIndex-itemPerPage;
-const currentNews=news.slice(firstIndex,lastIndex);
-const totalPages=Math.ceil(news.length/itemPerPage);
+  // pagination
+  const lastIndex = itemPerPage * currentPage;
+  const firstIndex = lastIndex - itemPerPage;
+  const currentNews = news.slice(firstIndex, lastIndex);
+  const totalPages = Math.ceil(news.length / itemPerPage);
 
- 
- if (loading) return <h1>Loading....</h1>;
+  if (loading) return <h1>Loading....</h1>;
   if (error) return <h1>{error}</h1>;
-  if (news.length === 0) return <h2>No News Found </h2>;
+  if (news.length === 0) return <h2>No News Found</h2>;
 
   return (
-    <div >
-      <h3 > Latest News</h3>
-      <input 
-  type="text"
-  placeholder="Search news..."
-  value={search}
-  onChange={(e) => setSearch(e.target.value)}
-/>
+    <div>
+      <h3>Latest News</h3>
 
- {currentNews.map((item, index) => (
+      <input
+        type="text"
+        placeholder="Search news..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <button onClick={fetchdata}>Search</button>
+
+      {currentNews.map((item, index) => (
         <div key={index}>
           <h3>{item.title}</h3>
           <p>{item.description}</p>
         </div>
       ))}
 
+      {/* pagination */}
+      <div>
         <button
           onClick={() => setCurrentPage(currentPage - 1)}
           disabled={currentPage === 1}
@@ -67,6 +71,28 @@ const totalPages=Math.ceil(news.length/itemPerPage);
           Prev
         </button>
 
+         {[...Array(totalPages)].map((_, i) => (     //agar yaha pr hm without spread operator array use krenge then aaray ke andar bydefault null hoga and map iterate nhi kr payega array pr thats why we use spread operator so that we can itrate
+          <button
+            key={i}
+            onClick={() => setCurrentPage(i + 1)}
+            style={{
+              margin: "5px",
+              fontWeight: currentPage === i + 1 ? "bold" : "normal",
+            }}
+          >
+            {i + 1}
+          </button>
+        ))}
+
+        <span> Page {currentPage} / {totalPages} </span>
+
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
