21ec21c94abcde72efdd620579a681dc14ccaff8
Vishal795-knightrider <vk3293801@gmail.com>
2026-04-10 15:44:55 +0530

    create new file Context api

---
+++ context-api/src/context.jsx
@@ -0,0 +1,8 @@
+import {createContext} from 'react';
+
+//Empty Context
+const ThemeComntext =createContext();
+const UserContext=createContext();
+
+// default context
+const LangContext=createContext("en");
